6ebdaab53454bd83aa41dfdef38de52fe39c970d
nicolascarrizook <nicolascarrizook@gmail.com>
2021-09-14 17:13:47 -0300

    Subida final

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+const client = contentful.createClient({
+    // This is the space ID. A space is like a project folder in Contentful terms
+    space: "z3w1dc5ojyng",
+    // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+    accessToken: "6l9_PWjfdLDad7MW34weCswswT6-bkVk8dOf05-HLF4"
+  });
+
 //Variables
 
 const cartBtn = document.querySelector('.cart-btn');
@@ -20,9 +27,15 @@ let buttonsDom = [];
 class Products {
     async getProducts(){
         try {
-            let res = await fetch('product.json');
-            let data = await res.json();
-            let products = data.items;
+
+            let contentful = await client.getEntries({
+                content_type: 'javascriptFinalCoder'
+            });
+
+            // let res = await fetch('product.json');
+            // let data = await res.json();
+            
+            let products = contentful.items;
             products = products.map( item => {
                 const {title, price} = item.fields;
                 const {id} = item.sys;
